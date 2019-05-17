de2af82108861620736be252952d9bc96a92c5cb
Weihao Qu <weihaoqu@Weihaos-MBP.fios-router.home>
2019-05-16 23:50:08 -0400

    whysolver

--- BiArel_code_icfp_0515-core-0.12/src/whySolver.ml
+++ BiArel_code_icfp_0515-core-0.12/src/whySolver.ml
@@ -311,7 +311,7 @@ let send_smt cs =
   why_debug dp "!*! -----------------------------------------------";
   
    let res =  post why3_cs 1 in
-  (* why_debug dp "Time for SMT solver:%f\n"  !smt_time; res*)
+   why_debug dp "Time for SMT solver:%f\n"  !smt_time; res
 
   let send_smt_u cs =
   let t = Unix.gettimeofday () in
@@ -335,4 +335,4 @@ let send_smt cs =
   why_debug dp "!*! -----------------------------------------------";
   
    let res =  post why3_cs 1 in
-   (*why_debug dp "Time for SMT solver:%f\n" !smt_time ; res *)
+   why_debug dp "Time for SMT solver:%f\n" !smt_time ; res 
